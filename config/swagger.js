8a669447713215480d67062838b9957ea58d004f
beatuslemi0-coder <beatuslemi0@gmail.com>
2026-08-12 16:11:42 +0300

    Fix Swagger server URL

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000"
+                url: "https://book-api-ejt0.onrender.com"
             },
         ],
         components: {
