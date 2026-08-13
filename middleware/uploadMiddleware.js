7ee749e0d6eaccb0b111f4f1d3b91ae532d7ad5e
beatuslemi0-coder <beatuslemi0@gmail.com>
2026-08-13 18:28:47 +0300

    Fix Cloudinary  storage configuraton

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -2,12 +2,21 @@
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name:
+    process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECTET,
+});
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: "book-commerce",
-        allowed_formats:["jpg","jpeg","png"],
+        folder: "book_cover",
+        allowed_formats:["jpg","jpeg","png","webp"],
+        transformation:[{width:500, height:700,crop: 'limit'}],
     },
 });
 const fileFilter = (req, file, cb) => {
@@ -33,4 +42,4 @@ const upload = multer({
         fileSize: 5 * 1024 *1024 
     }
 });
-module.exports = upload;
+module.exports = {cloudinary,upload};
